bea333c48b15a71056af88d7e52fe9234517dc45
p.chernyak <p.chernyak@qiwi.com>
2024-10-24 19:21:33 +0300

    Add nvim dotfiles

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,32 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local workspace_dir = '/home/fjfalcon/dev/workspaces/' .. project_name
+
+local config = {
+		 cmd = {
+				'java',
+				'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+				'-Dosgi.bundles.defaultStartLevel=4',
+				'-Declispe.product=org.eclipse.jdt.ls.core.product',
+				'-Dlog.protocl=true',
+				'-Dlog.level=ALL',
+				'-Xmx4g',
+				'--add-modules=ALL-SYSTEM',
+				'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+				'--add-opens', 'java.base/java.lang==ALL-UNNAMED',
+				'-jar' , '/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+				'-configuration', '/usr/share/java/jdtls/config_linux',
+				'-data', workspace_dir,	
+			 },
+			 root_dir = vim.fs.root(0, { ".git"} ),
+			 settings = {
+				java = {
+				}
+	 		 },
+
+			 init_options = {
+				 bundles = {}
+			 },
+}
+
+require('jdtls').start_or_attach(config)
---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("fjfalcon/mapping")
+require("fjfalcon/configs")
+require("config.lazy")
