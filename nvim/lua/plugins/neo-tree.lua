be994808479d49ce4b746dac571e1469da2f4373
fjfalcon <fjfalcon@fjfalcon.ru>
2024-10-24 19:21:33 +0300

    Add nvim dotfiles

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+	{ 
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependecies = {
+			"nvia-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		}
+	},
+}
