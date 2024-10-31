73e4c017b34835e4f0b1e4e324971dd947cb5828
fjfalcon <fjfalcon@fjfalcon.ru>
2024-10-31 13:57:41 +0300

    Move mappings to mapping.lua

--- nvim/lua/fjfalcon/mapping.lua
+++ nvim/lua/fjfalcon/mapping.lua
@@ -1,3 +1,5 @@
+-- neotree
+
 vim.keymap.set(
 	"n",
 	"<A-1>",
@@ -10,6 +12,7 @@ vim.keymap.set(
 	"<Cmd>Neotree toggle position=left<CR>"
 )
 
+-- windows
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
@@ -18,6 +21,7 @@ vim.keymap.set("n", "<F2>", vim.diagnostic.goto_next, { desc = "Go to next error
 vim.keymap.set("n", "<leader>wv", ":vsplit<CR>", { desc = "[W]indow Split [V]ertical" })
 vim.keymap.set("n", "<leader>wh", ":split<CR>", { desc = "[W]indow Split [V]ertical" })
 
+-- terminal
 local api = vim.api
 
 api.nvim_command("autocmd TermOpen * startinsert") -- starts in insert mode
@@ -27,3 +31,32 @@ api.nvim_command("autocmd TermEnter * setlocal signcolumn=no") -- no sign column
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
 
 vim.keymap.set({ "n", "i" }, "<F12>", ":bot term<CR>", { noremap = true, silent = true, desc = "Open terminal" })
+
+-- telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "leader>cd", vim.lsp.buf.definition, { desc = "[C]ode goto [D]efinition" })
+vim.keymap.set({ "n", "v" }, "leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]efinition" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>e", builtin.oldfiles, { desc = "Telescope Lists previously open files" })
+vim.keymap.set("n", "leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode goto [R]eferences" })
+vim.keymap.set("n", "leader>ci", require("telescope.builtin").lsp_references, { desc = "[C]ode goto [I]mplementation" })
+
+-- which-key
+vim.keymap.set("n", "<leader>?", function()
+	require("which-key").show({ global = false })
+end, {
+	desc = "Buffer Local Keymaps (which-key)",
+})
+
+-- conform
+vim.keymap.set("n", "<leader>f", function()
+	require("conform").format({ async = true, lsp_format = "fallback" })
+end, { desc = "[F]ormat buffer" })
+
+-- lspconfig
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Goto implementation" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "Rename" })
+vim.keymap.set("n", "<leader>o", vim.lsp.buf.format, { desc = "Format code" })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,23 +3,6 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
 		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "leader>cd", vim.lsp.buf.definition, { desc = "[C]ode goto [D]efinition" })
-			vim.keymap.set({ "n", "v" }, "leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]efinition" })
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>e", builtin.oldfiles, { desc = "Telescope Lists previously open files" })
-			vim.keymap.set(
-				"n",
-				"leader>cr",
-				require("telescope.builtin").lsp_references,
-				{ desc = "[C]ode goto [R]eferences" }
-			)
-			vim.keymap.set(
-				"n",
-				"leader>ci",
-				require("telescope.builtin").lsp_references,
-				{ desc = "[C]ode goto [I]mplementation" }
-			)
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = { require("telescope.themes").get_dropdown() },
